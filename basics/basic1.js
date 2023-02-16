72e5a389f6faeb839b5a7688e81fa16f313ed971
samirvithlani <samir.vithlani83955@gmail.com>
2023-02-16 12:58:17 +0530

    basic

---
+++ basics/basic1.js
@@ -0,0 +1,13 @@
+console.log("Hello World")
+console.error("This is an error")
+console.warn("This is a warning")
+
+
+var a  = 15
+let x = 250
+const ifsc = "SBIN0000001"
+
+console.log("value of a = ",a)
+console.log("value of x = ",x)
+ifsc = "SBIN0000002"
+console.log("value of ifsc = ",ifsc)
---
+++ basics/operartors/operator1.js
@@ -0,0 +1,19 @@
+var x = 100
+console.log("value of x = ",x)
+
+var a = 10
+var b = 20
+var temp
+
+
+temp =a // tenp = 10
+a = b // a = 20
+b = temp // b = 10
+
+console.log("value of a = ",a)
+console.log("value of b = ",b)
+
+var x = 250
+//x = x + 100
+x+=100
+console.log("value of x = ",x)
